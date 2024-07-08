baf273b50fea96f4cfa3e21092dba2fd1fa9e57e
Felipe Santos <felipe.carvalho.bs.contato@gmail.com>
2024-07-08 01:01:29 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/fastfood/api/FastfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fastfood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodApiApplication.class, args);
+	}
+
+}
