8978d991f50f0c89d0a6ad6fa8c6158a80bb320a
Felipe Santos <felipe.carvalho.bs.contato@gmail.com>
2024-07-25 19:02:32 -0300

    48 - Enhancement - Changed activation method to GET

--- src/main/java/br/com/fiap/fastfood/api/adapters/driver/controller/ActivationCodeController.java
+++ src/main/java/br/com/fiap/fastfood/api/adapters/driver/controller/ActivationCodeController.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,7 @@ public class ActivationCodeController {
     this.customerService = customerService;
   }
 
-  @PatchMapping(value = "/{code}")
+  @GetMapping(value = "/{code}")
   public ResponseEntity<Void> activate(@PathVariable UUID code) {
     customerService.activate(code);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
