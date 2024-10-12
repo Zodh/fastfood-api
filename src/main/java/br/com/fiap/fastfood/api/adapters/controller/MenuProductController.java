a0e0e4eb2e900e28cea0db32336fda26e2e25fde
RobsonArcoleze <robson.arcoleze29@gmail.com>
2024-10-11 22:44:00 -0300

    #79 - Fix - Corrigido camada de controllers

--- src/main/java/br/com/fiap/fastfood/api/adapters/controller/MenuProductController.java
+++ src/main/java/br/com/fiap/fastfood/api/adapters/controller/MenuProductController.java
@@ -20,48 +20,43 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/menu-products")
+
 public class MenuProductController {
 
   private final MenuProductService menuProductService;
 
-  @Autowired
+
   public MenuProductController(
       MenuProductRepositoryGatewayImpl menuProductRepositoryAdapter) {
     this.menuProductService = new MenuProductServiceImpl(menuProductRepositoryAdapter, new MenuProductMapperAppImpl());
   }
 
-  @GetMapping
-  public ResponseEntity<MenuProductResponseDTO> getAll() {
+  public MenuProductResponseDTO getAll() {
     List<MenuProductDTO> allProducts = menuProductService.getAll();
     MenuProductResponseDTO response = new MenuProductResponseDTO(allProducts);
-    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    return response;
   }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<MenuProductDTO> getById(@PathVariable Long id) {
+
+  public MenuProductDTO getById(Long id) {
     MenuProductDTO menuProductDTO = menuProductService.getById(id);
-    return ResponseEntity.status(HttpStatus.CREATED).body(menuProductDTO);
+    return menuProductDTO;
   }
 
-  @PostMapping
-  public ResponseEntity<Void> create(@Valid @RequestBody MenuProductDTO menuProductDTO) {
+
+  public void create(MenuProductDTO menuProductDTO) {
     menuProductService.register(menuProductDTO);
-    return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
-  @PutMapping("/{id}")
-  public ResponseEntity<Void> update(@PathVariable Long id,
+
+  public void update(Long id,
       @RequestBody MenuProductDTO menuProductDTO) {
     menuProductService.update(id, menuProductDTO);
-    return ResponseEntity.status(HttpStatus.OK).build();
   }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+  public void delete(@PathVariable Long id) {
     menuProductService.remove(id);
-    return ResponseEntity.status(HttpStatus.OK).build();
   }
 
 }
